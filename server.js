cc6ed701c4a173f296bb608bbe234bb4256499d0
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-16 22:16:43 -0700

    set up server to listen

--- server.js
+++ server.js
@@ -11,3 +11,11 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+
+
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
