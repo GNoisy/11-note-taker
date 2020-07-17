cfee6c1a8f6784621cbfee06feba4eb186439920
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-17 15:52:41 -0700

    set app.get routes for notes

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
 
 // Starts the server to begin listening
